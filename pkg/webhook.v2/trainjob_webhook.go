830cc0bff62bd6d2dbf533b676c25a242cf72bca
Akshay Chitneni <achitneni@apple.com>
2024-10-04 08:02:40 -0700

    Adding v2 tranjob webhook

--- pkg/webhook.v2/trainjob_webhook.go
+++ pkg/webhook.v2/trainjob_webhook.go
@@ -18,6 +18,15 @@ package webhookv2
 
 import (
 	"context"
+	"fmt"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"slices"
+	"strconv"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -27,12 +36,23 @@ import (
 	kubeflowv2 "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v2alpha1"
 )
 
-type TrainJobWebhook struct{}
+var (
+	specPath                = field.NewPath("spec")
+	trainingRuntimeRefPath  = specPath.Child("trainingRuntimeRef")
+	trainingRuntimeNamePath = trainingRuntimeRefPath.Child("name")
+)
+
+type TrainJobWebhook struct {
+	Client client.Client
+}
 
 func setupWebhookForTrainJob(mgr ctrl.Manager) error {
+	webhook := &TrainJobWebhook{
+		Client: mgr.GetClient(),
+	}
 	return ctrl.NewWebhookManagedBy(mgr).
 		For(&kubeflowv2.TrainJob{}).
-		WithValidator(&TrainJobWebhook{}).
+		WithValidator(webhook).
 		Complete()
 }
 
@@ -40,14 +60,167 @@ func setupWebhookForTrainJob(mgr ctrl.Manager) error {
 
 var _ webhook.CustomValidator = (*TrainJobWebhook)(nil)
 
-func (w *TrainJobWebhook) ValidateCreate(context.Context, runtime.Object) (admission.Warnings, error) {
-	return nil, nil
+func (w *TrainJobWebhook) ValidateCreate(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
+	trainJob := obj.(*kubeflowv2.TrainJob)
+	log := ctrl.LoggerFrom(ctx).WithName("trainJob-webhook")
+	log.V(5).Info("Validating create", "TrainJob", klog.KObj(trainJob))
+	return nil, validateTrainJob(ctx, w.Client, nil, trainJob).ToAggregate()
 }
 
-func (w *TrainJobWebhook) ValidateUpdate(context.Context, runtime.Object, runtime.Object) (admission.Warnings, error) {
-	return nil, nil
+func (w *TrainJobWebhook) ValidateUpdate(ctx context.Context, old runtime.Object, new runtime.Object) (admission.Warnings, error) {
+	oldTrainJob := old.(*kubeflowv2.TrainJob)
+	newTrainJob := new.(*kubeflowv2.TrainJob)
+	log := ctrl.LoggerFrom(ctx).WithName("trainJob-webhook")
+	log.V(5).Info("Validating update", "TrainJob", klog.KObj(newTrainJob))
+	return nil, validateTrainJob(ctx, w.Client, oldTrainJob, newTrainJob).ToAggregate()
 }
 
 func (w *TrainJobWebhook) ValidateDelete(context.Context, runtime.Object) (admission.Warnings, error) {
 	return nil, nil
+}
+
+func validateTrainJob(ctx context.Context, client client.Client, oldJob, newJob *kubeflowv2.TrainJob) field.ErrorList {
+	var allErrs field.ErrorList
+	var trainingRuntime kubeflowv2.TrainingRuntime
+	trainingRuntime.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   *newJob.Spec.TrainingRuntimeRef.APIGroup,
+		Version: "v1alpha1",
+		Kind:    *newJob.Spec.TrainingRuntimeRef.Kind,
+	})
+	var key types.NamespacedName
+	if *newJob.Spec.TrainingRuntimeRef.Kind == "ClusterTrainingRuntime" {
+		key = types.NamespacedName{
+			Name: newJob.Spec.TrainingRuntimeRef.Name,
+		}
+	} else {
+		key = types.NamespacedName{
+			Name:      newJob.Spec.TrainingRuntimeRef.Name,
+			Namespace: newJob.Namespace,
+		}
+	}
+	if err := client.Get(ctx, key, &trainingRuntime); err != nil {
+		if errors.IsNotFound(err) {
+			allErrs = append(allErrs, field.NotFound(trainingRuntimeNamePath, newJob.Spec.TrainingRuntimeRef.Name))
+		} else {
+			allErrs = append(allErrs, field.InternalError(trainingRuntimeNamePath, err))
+		}
+		return allErrs
+	}
+
+	allErrs = append(allErrs, validateTrainJobSpec(&newJob.Spec, &trainingRuntime)...)
+	return allErrs
+}
+
+func validateTrainJobSpec(spec *kubeflowv2.TrainJobSpec, trainingRuntime *kubeflowv2.TrainingRuntime) field.ErrorList {
+	var allErrs field.ErrorList
+	if spec.Trainer != nil {
+		numProcPerNodePath := specPath.Child("trainer").Child("numProcPerNode")
+		if trainingRuntime.Spec.MLPolicy.MPI != nil {
+			if _, err := strconv.Atoi(*spec.Trainer.NumProcPerNode); err != nil {
+				allErrs = append(allErrs, field.Invalid(numProcPerNodePath, spec.Trainer.NumProcPerNode, "should have an int value"))
+			}
+		} else if trainingRuntime.Spec.MLPolicy.Torch != nil {
+			allowedStringValList := []string{"auto", "cpu", "gpu"}
+			if !slices.Contains(allowedStringValList, *spec.Trainer.NumProcPerNode) {
+				if _, err := strconv.Atoi(*spec.Trainer.NumProcPerNode); err != nil {
+					allErrs = append(allErrs, field.Invalid(numProcPerNodePath, spec.Trainer.NumProcPerNode, "should have an int value or auto/cpu/gpu"))
+				}
+			}
+		}
+	}
+
+	if spec.ModelConfig != nil {
+		if spec.ModelConfig.Input != nil {
+			modelConfigInputPath := specPath.Child("modelConfig").Child("input")
+			if len(trainingRuntime.Spec.Template.Spec.ReplicatedJobs) == 0 {
+				allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Input, "trainingRuntime should have replicated jobs configured with model config input set"))
+			} else {
+				initializerJobFound := false
+				modelInitializerContainerFound := false
+				for _, job := range trainingRuntime.Spec.Template.Spec.ReplicatedJobs {
+					if job.Name == "Initializer" {
+						initializerJobFound = true
+						for _, container := range job.Template.Spec.Template.Spec.Containers {
+							if container.Name == "model-initializer" {
+								modelInitializerContainerFound = true
+							}
+						}
+					}
+				}
+				if !initializerJobFound {
+					allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Input, "trainingRuntime should have replicated job configured with name - Initializer"))
+				} else if !modelInitializerContainerFound {
+					allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Input, "trainingRuntime with replicated job initializer should have container with name - model-initializer"))
+				}
+			}
+		}
+		if spec.ModelConfig.Output != nil {
+			modelConfigInputPath := specPath.Child("modelConfig").Child("output")
+			if len(trainingRuntime.Spec.Template.Spec.ReplicatedJobs) == 0 {
+				allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Output, "trainingRuntime should have replicated jobs configured with model config output set"))
+			} else {
+				exporterJobFound := false
+				modelExporterContainerFound := false
+				for _, job := range trainingRuntime.Spec.Template.Spec.ReplicatedJobs {
+					if job.Name == "Exporter" {
+						exporterJobFound = true
+						for _, container := range job.Template.Spec.Template.Spec.Containers {
+							if container.Name == "model-exporter" {
+								modelExporterContainerFound = true
+							}
+						}
+					}
+				}
+				if !exporterJobFound {
+					allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Input, "trainingRuntime should have replicated job configured with name - Exporter"))
+				} else if !modelExporterContainerFound {
+					allErrs = append(allErrs, field.Invalid(modelConfigInputPath, spec.ModelConfig.Input, "trainingRuntime with replicated job initializer should have contianer with name - model-exporter"))
+				}
+			}
+		}
+	}
+
+	if len(spec.PodSpecOverrides) != 0 {
+		podSpecOverridesPath := specPath.Child("podSpecOverrides")
+		jobsMap := map[string]bool{}
+		for _, job := range trainingRuntime.Spec.Template.Spec.ReplicatedJobs {
+			jobsMap[job.Name] = true
+		}
+		for idx, override := range spec.PodSpecOverrides {
+			for _, job := range override.TargetReplicatedJobs {
+				if ok, _ := jobsMap[job]; !ok {
+					allErrs = append(allErrs, field.Invalid(podSpecOverridesPath, spec.PodSpecOverrides, fmt.Sprintf("job: %s, configured in the podOverride should be present in the referenced training runtime", job)))
+				}
+			}
+			if len(override.Containers) != 0 {
+				containerMap := map[string]bool{}
+				for _, job := range trainingRuntime.Spec.Template.Spec.ReplicatedJobs {
+					for _, container := range job.Template.Spec.Template.Spec.Containers {
+						containerMap[container.Name] = true
+					}
+				}
+				containerOverridePath := podSpecOverridesPath.Index(idx)
+				for _, container := range override.Containers {
+					if _, ok := containerMap[container.Name]; !ok {
+						allErrs = append(allErrs, field.Invalid(containerOverridePath, override.Containers, fmt.Sprintf("container: %s, configured in the containerOverride should be present in the referenced training runtime", container.Name)))
+					}
+				}
+			}
+			if len(override.InitContainers) != 0 {
+				initContainerMap := map[string]bool{}
+				for _, job := range trainingRuntime.Spec.Template.Spec.ReplicatedJobs {
+					for _, initContainer := range job.Template.Spec.Template.Spec.InitContainers {
+						initContainerMap[initContainer.Name] = true
+					}
+				}
+				initContainerOverridePath := podSpecOverridesPath.Index(idx)
+				for _, container := range override.Containers {
+					if _, ok := initContainerMap[container.Name]; !ok {
+						allErrs = append(allErrs, field.Invalid(initContainerOverridePath, override.InitContainers, fmt.Sprintf("initContainer: %s, configured in the initContainerOverride should be present in the referenced training runtime", container.Name)))
+					}
+				}
+			}
+		}
+	}
+	return allErrs
 }
--- test/integration/webhook.v2/trainjob_test.go
+++ test/integration/webhook.v2/trainjob_test.go
@@ -17,16 +17,20 @@ limitations under the License.
 package webhookv2
 
 import (
+	kubeflowv2 "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v2alpha1"
+	"github.com/kubeflow/training-operator/test/integration/framework"
 	"github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/kubeflow/training-operator/test/integration/framework"
+	jobsetv1alpha2 "sigs.k8s.io/jobset/api/jobset/v1alpha2"
 )
 
 var _ = ginkgo.Describe("TrainJob Webhook", ginkgo.Ordered, func() {
 	var ns *corev1.Namespace
+	apiGroup := "kubeflow.org"
+	runtimeKind := "TrainingRuntime"
 
 	ginkgo.BeforeAll(func() {
 		fwk = &framework.Framework{}
@@ -48,5 +52,220 @@ var _ = ginkgo.Describe("TrainJob Webhook", ginkgo.Ordered, func() {
 			},
 		}
 		gomega.Expect(k8sClient.Create(ctx, ns)).To(gomega.Succeed())
+	})
+
+	ginkgo.It("Should succeed in creating trainJob with trainingRuntime present", func() {
+
+		runtimeName := "valid"
+		trainingRuntime := kubeflowv2.TrainingRuntime{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       runtimeKind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      runtimeName,
+				Namespace: ns.Name,
+			},
+		}
+		gomega.Expect(k8sClient.Create(ctx, &trainingRuntime)).To(gomega.Succeed())
+
+		trainingRuntimeRef := kubeflowv2.TrainingRuntimeRef{
+			Name:     runtimeName,
+			APIGroup: &apiGroup,
+			Kind:     &runtimeKind,
+		}
+		jobSpec := kubeflowv2.TrainJobSpec{
+			TrainingRuntimeRef: trainingRuntimeRef,
+		}
+		trainJob := &kubeflowv2.TrainJob{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       "TrainJob",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "valid-trainjob",
+				Namespace: ns.Name,
+			},
+			Spec: jobSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, trainJob)).To(gomega.Succeed())
+	})
+
+	ginkgo.It("Should fail in creating trainJob with trainingRuntime not present", func() {
+
+		runtimeName := "inValid"
+
+		trainingRuntimeRef := kubeflowv2.TrainingRuntimeRef{
+			Name:     runtimeName,
+			APIGroup: &apiGroup,
+			Kind:     &runtimeKind,
+		}
+		jobSpec := kubeflowv2.TrainJobSpec{
+			TrainingRuntimeRef: trainingRuntimeRef,
+		}
+		trainJob := &kubeflowv2.TrainJob{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       "TrainJob",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "valid-trainjob",
+				Namespace: ns.Name,
+			},
+			Spec: jobSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, trainJob)).To(gomega.HaveOccurred())
+	})
+
+	ginkgo.It("Should fail in creating trainJob with pre-trained model config when referencing "+
+		"a trainingRuntime without an initializer", func() {
+
+		runtimeName := "runtime-without-initializer"
+		trainingRuntime := kubeflowv2.TrainingRuntime{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       runtimeKind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      runtimeName,
+				Namespace: ns.Name,
+			},
+		}
+		gomega.Expect(k8sClient.Create(ctx, &trainingRuntime)).To(gomega.Succeed())
+
+		trainingRuntimeRef := kubeflowv2.TrainingRuntimeRef{
+			Name:     runtimeName,
+			APIGroup: &apiGroup,
+			Kind:     &runtimeKind,
+		}
+		inputModel := &kubeflowv2.InputModel{}
+		jobSpec := kubeflowv2.TrainJobSpec{
+			TrainingRuntimeRef: trainingRuntimeRef,
+			ModelConfig:        &kubeflowv2.ModelConfig{Input: inputModel},
+		}
+		trainJob := &kubeflowv2.TrainJob{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       "TrainJob",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "invalid-trainjob-",
+				Namespace:    ns.Name,
+			},
+			Spec: jobSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, trainJob)).
+			To(gomega.MatchError(gomega.ContainSubstring("admission webhook \"validator.trainjob.kubeflow.org\" " +
+				"denied the request: spec.modelConfig.input")))
+	})
+
+	ginkgo.It("Should fail in creating trainJob with output model config when referencing a trainingRuntime"+
+		" without an exporter", func() {
+
+		runtimeName := "runtime-without-initializer"
+		trainingRuntime := kubeflowv2.TrainingRuntime{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       runtimeKind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      runtimeName,
+				Namespace: ns.Name,
+			},
+		}
+		gomega.Expect(k8sClient.Create(ctx, &trainingRuntime)).To(gomega.Succeed())
+
+		trainingRuntimeRef := kubeflowv2.TrainingRuntimeRef{
+			Name:     runtimeName,
+			APIGroup: &apiGroup,
+			Kind:     &runtimeKind,
+		}
+		outputModel := &kubeflowv2.OutputModel{}
+		jobSpec := kubeflowv2.TrainJobSpec{
+			TrainingRuntimeRef: trainingRuntimeRef,
+			ModelConfig:        &kubeflowv2.ModelConfig{Output: outputModel},
+		}
+		trainJob := &kubeflowv2.TrainJob{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       "TrainJob",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "invalid-trainjob-",
+				Namespace:    ns.Name,
+			},
+			Spec: jobSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, trainJob)).
+			To(gomega.MatchError(gomega.ContainSubstring("admission webhook \"validator.trainjob.kubeflow.org\" " +
+				"denied the request: spec.modelConfig.output")))
+	})
+
+	ginkgo.It("Should fail in creating trainJob with podSpecOverrides when referencing a trainingRuntime doesnt "+
+		"have the job specified in the override", func() {
+
+		runtimeName := "runtime-with-replicatedjob"
+		trainingRuntimeSpec := kubeflowv2.TrainingRuntimeSpec{
+			Template: kubeflowv2.JobSetTemplateSpec{
+				Spec: jobsetv1alpha2.JobSetSpec{
+					ReplicatedJobs: []jobsetv1alpha2.ReplicatedJob{
+						{
+							Name: "valid",
+							Template: batchv1.JobTemplateSpec{
+								Spec: batchv1.JobSpec{
+									Template: corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											Containers: []corev1.Container{
+												{
+													Name: "initializer",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+		trainingRuntime := kubeflowv2.TrainingRuntime{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       runtimeKind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      runtimeName,
+				Namespace: ns.Name,
+			},
+			Spec: trainingRuntimeSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, &trainingRuntime)).To(gomega.Succeed())
+
+		trainingRuntimeRef := kubeflowv2.TrainingRuntimeRef{
+			Name:     runtimeName,
+			APIGroup: &apiGroup,
+			Kind:     &runtimeKind,
+		}
+		jobSpec := kubeflowv2.TrainJobSpec{
+			TrainingRuntimeRef: trainingRuntimeRef,
+			PodSpecOverrides: []kubeflowv2.PodSpecOverride{
+				{TargetReplicatedJobs: []string{"valid", "invalid"}},
+			},
+		}
+		trainJob := &kubeflowv2.TrainJob{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: kubeflowv2.SchemeGroupVersion.String(),
+				Kind:       "TrainJob",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				GenerateName: "invalid-trainjob-",
+				Namespace:    ns.Name,
+			},
+			Spec: jobSpec,
+		}
+		gomega.Expect(k8sClient.Create(ctx, trainJob)).
+			To(gomega.MatchError(gomega.ContainSubstring("admission webhook \"validator.trainjob.kubeflow.org\" " +
+				"denied the request: spec.podSpecOverrides")))
 	})
 })
